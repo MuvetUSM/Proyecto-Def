afe38e0e811f49a5b991ae257c9921983c04aead
G10v4l4rc0n <giovanni.alarcon@usm.cl>
2024-07-01 22:22:21 -0400

    Puede ser pa?

--- Copia Proyecto Testeo/MuvetUSM/apps/Core/views.py
+++ Copia Proyecto Testeo/MuvetUSM/apps/Core/views.py
@@ -5,7 +5,6 @@ from django.contrib.auth.forms import PasswordResetForm
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
-<<<<<<< HEAD
 from .models import Usuario
 from .forms import RegistroUsuario, IniciarUsuario
 
@@ -47,46 +46,3 @@ class resetForm(PasswordResetForm):
         # Add any custom logic here, e.g., clearing session data
         return super().get(request)
 """
-=======
-from .models import Usuario, Curso
-from .forms import RegistroUsuario, IniciarUsuario, EditCursoForm
-
-def registrar(request):
-    data = {
-        'form': RegistroUsuario()
-    }
-
-    if request.method == "POST":
-        user_creation_form = RegistroUsuario(data=request.POST)
-        if user_creation_form.is_valid():
-            user_creation_form.save()
-            return redirect('home')
-
-    return render(request, 'Core/registrodeUsuario.html', data)
-
-def home(request):
-    return render(request, 'Core/home.html')
-
-def cursos(request):
-    cursos = Curso.objects.all()
-    return render(request, 'Core/cursos.html', {'cursos': cursos})
-
-def edicion(request, name):
-    curso = get_object_or_404(Curso, name=name)
-    
-    if request.method == 'POST':
-        form = EditCursoForm(request.POST, instance=curso)
-        if form.is_valid():
-            form.save()
-            return redirect('cursos') 
-    else:
-        form = EditCursoForm(instance=curso)
-    
-    return render(request, 'Core/edicion.html', {'form': form})
-
-
-def eliminar(request, name):
-    curso = get_object_or_404(Curso, name=name)
-    curso.delete()
-    return redirect('cursos') 
->>>>>>> cd6759f556e37cd4d39bc74157f901c56c2c9a88
